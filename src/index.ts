00fbfcba9f6292c18e206c11293ab1060d708035
Jhonatan Caicedo <jonatan9911@gmail.com>
2026-07-18 14:29:16 -0500

    feat(002): add utils file

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,14 @@
 //     .then(response => response.json())
 //     .then(data => console.log("Fetched data:", data.userId))
 
-// console.log("Data fetched successfully!");
 
 import axios from "axios"
 
 const main = async () => {
 
     const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
+
+    console.log("Fetched data:", response.data.userId)
     
     console.log(response.data)
 }
